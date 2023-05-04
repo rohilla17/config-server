6b604382befacd5ec501031cbab82ec23359c725
Shubham Rohilla <shubham.rohilla@sastechstudio.com>
2023-05-04 11:57:55 +0530

    initial commit

---
+++ src/main/java/com/sts/configserverpoc/ConfigServerPocApplication.java
@@ -0,0 +1,15 @@
+package com.sts.configserverpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sts/configserverpoc/ConfigServerPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sts.configserverpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
