46a1afc887b72946ca22b599e963a0aa616c3c28
Shubham Rohilla <shubham.rohilla@sastechstudio.com>
2023-05-04 12:20:34 +0530

    finncub-dev

--- src/main/java/com/sts/configserverpoc/ConfigServerPocApplication.java
+++ src/main/java/com/sts/configserverpoc/ConfigServerPocApplication.java
@@ -1,11 +1,15 @@
 package com.sts.configserverpoc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableConfigServer
+@Configuration
+@EnableAutoConfiguration
 public class ConfigServerPocApplication {
 
 	public static void main(String[] args) {
